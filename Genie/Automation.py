a51485e83f08a869d57c5d02eff431136eeedec4
Designer225 <designer2553@gmail.com>
2024-05-04 16:46:50 -0700

    Updated automation script
    Now can use context and expected output nodes.
    
    Also added type hints. They are cool.

--- Genie/Automation.py
+++ Genie/Automation.py
@@ -2,6 +2,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver.webdriver import WebDriver
 
 from Driver import get_driver, close_driver
 from ParseMaster import read_questions
@@ -15,13 +16,15 @@ def run_automation(test_data_path, output_dir, output_file):
     responses = []
 
     writer = initialize_output(output_dir, output_file)  # Initialize output file
-
-    for i in range(0, len(questions), 5):  # Process in batches of 10
+    
+    for i in range(0, len(questions), 5):  # Process in batches of 5
         driver = get_driver()  # Get a new driver instance
         setting_up(driver)
         # Process each question in the current batch
-        for question in questions[i:i + 5]:
-            send_question(driver, question)
+        for _, question in questions.iloc[i : i + 5].iterrows():
+        # for question in questions['input'][i:i + 5]:
+            print(question)
+            send_question(driver, question['input'])
             response = get_response(driver)
             responses.append({'Response': response})
             print(response)
@@ -32,7 +35,7 @@ def run_automation(test_data_path, output_dir, output_file):
     # close_driver(driver)
     save_response(writer, responses)  # Save all responses to the Excel file
     # finalize_output(writer)
-def navigate_back(driver):
+def navigate_back(driver: WebDriver):
     # el2 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
     #                           value="new UiSelector().className(\"android.view.View\").instance(6)")
     # el2.click()
@@ -42,34 +45,53 @@ def navigate_back(driver):
 
     el2.click()
 
-def setting_up(driver):
+def setting_up(driver: WebDriver):
     time.sleep(2)
     el1 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue")
     el1.click()
     el2 = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Continue")
     el2.click()
     time.sleep(1)
-    el3 = driver.find_element(by=AppiumBy.ID, value="com.android.permissioncontroller:id/permission_allow_button")
-    el3.click()
+    try:
+        el3 = driver.find_element(by=AppiumBy.ID, value="com.android.permissioncontroller:id/permission_allow_button")
+        el3.click()
+    except:
+        pass
 
-def send_question(driver, question):
+def send_question(driver: WebDriver, question: str):
 
     el4 = driver.find_element(by=AppiumBy.XPATH, value="//android.widget.FrameLayout//android.widget.EditText")
     el4.click()
     el5 = driver.find_element(by=AppiumBy.XPATH, value="//android.widget.FrameLayout//android.widget.EditText")
     el5.send_keys(question)
-    el6 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
-                              value="new UiSelector().className(\"android.widget.Button\").instance(1)")
+    try:
+        el6 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
+                                  value="new UiSelector().className(\"android.widget.Button\").instance(1)")
+    except: # try again after a second
+        time.sleep(1)
+        el6 = driver.find_element(by=AppiumBy.ANDROID_UIAUTOMATOR,
+                                  value="new UiSelector().className(\"android.widget.Button\").instance(1)")
     el6.click()
     time.sleep(2)
     el9 = driver.find_element(by=AppiumBy.XPATH,
                               value="//android.widget.FrameLayout/android.widget.LinearLayout//android.widget.ImageView[@index='2']")
     captured_text = el9.get_attribute("content-desc")
+    if (not captured_text): # try again after 3 seconds if value is null
+        time.sleep(3)
+        el9 = driver.find_element(by=AppiumBy.XPATH,
+                                  value="//android.widget.FrameLayout/android.widget.LinearLayout//android.widget.ImageView[@index='2']")
+        captured_text = el9.get_attribute("content-desc")
+        if (not captured_text): # try again after 5 seconds if value is null
+            time.sleep(5)
+            el9 = driver.find_element(by=AppiumBy.XPATH,
+                                      value="//android.widget.FrameLayout/android.widget.LinearLayout//android.widget.ImageView[@index='2']")
+            captured_text = el9.get_attribute("content-desc")
+            # if result is still null, will be recorded as null
     # input_field = driver.find_element(by=AppiumBy.XPATH, value="//android.widget.FrameLayout//android.widget.EditText")
     # input_field.send_keys(question)
 
 
-def get_response(driver):
+def get_response(driver: WebDriver):
     # response = driver.find_element(by=AppiumBy.XPATH,value="//android.widget.FrameLayout//android.widget.TextView").text
     el9 = driver.find_element(by=AppiumBy.XPATH,
                               value="//android.widget.FrameLayout/android.widget.LinearLayout//android.widget.ImageView[@index='2']")
--- Genie/ParseMaster.py
+++ Genie/ParseMaster.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import json
 
-def read_questions(file_path):
+def read_questions(file_path: str):
     df = pd.read_excel(file_path)
-    return df['input'].head(10).tolist()  # Assuming the Excel column is named 'Question'
+    filtered = df[['input', 'contextDict', 'expoutput']].head(10)
+    filtered['contextDict'].update(filtered['contextDict'].apply(lambda x: json.loads(x.replace("'", "\""))))
+    filtered['expoutput'].update(filtered['expoutput'].apply(lambda x: json.loads(x.replace("'", "\""))))
+    # return df['input'].head(10).tolist()  # Assuming the Excel column is named 'Question'
+    return filtered.head(10)
