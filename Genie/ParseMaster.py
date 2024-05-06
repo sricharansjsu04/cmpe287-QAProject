56205bbf4bee55a747f6ea07f6fdc5c3f565a031
Designer225 <designer2553@gmail.com>
2024-05-05 19:22:50 -0700

    Update ParseMaster.py

--- Genie/ParseMaster.py
+++ Genie/ParseMaster.py
@@ -3,8 +3,7 @@ import json
 
 def read_questions(file_path: str):
     df = pd.read_excel(file_path)
-    filtered = df[['subkey', 'input', 'contextDict', 'expoutput']].head(10)
+    filtered = df[['subkey', 'input', 'contextDict', 'expoutput']]
     filtered['contextDict'] = filtered['contextDict'].apply(lambda x: json.loads(x.replace("'", "\"")))
     filtered['expoutput'] = filtered['expoutput'].apply(lambda x: json.loads(x.replace("'", "\"")))
-    # return df['input'].head(10).tolist()  # Assuming the Excel column is named 'Question'
-    return filtered.head(10)
+    return filtered
